c0831005370734853dde5ef1d6abda60243fa972
Meghdad Hadidi <meghdad.1987@gmail.com>
2019-01-25 23:27:38 +0330

    fixed refrence issue

--- components/layout.js
+++ components/layout.js
@@ -2,31 +2,31 @@ import { Container, Row, Col } from "reactstrap"
 
 // Components
 import Header from "./header"
-import MainItemsBlock from "./DynamicParts/MainItemsBlock"
-import PopularRecipeList from "./DynamicParts/PopularRecipeList"
-import VideoList from "./DynamicParts/VideoSlider"
+import MainItemsBlock from "./Widgets/MainItemsBlock"
+import PopularRecipeList from "./Widgets/PopularRecipeList"
+import VideoList from "./Widgets/VideoSlider"
 
 const Layout = () => (
-    <Container>
-        <Row>
-            <Col>
-                {/* <img src="../static/image/logo.png" alt="my image" /> */}
-                <Header />
-            </Col>
-        </Row>
-        <Row>
-            <Col />
-        </Row>
-        <Row>
-            <Col md={3}>
-                <PopularRecipeList />
-            </Col>
-            <Col md={9}>
-                <MainItemsBlock />
-                <VideoList />
-            </Col>
-        </Row>
-    </Container>
+	<Container>
+		<Row>
+			<Col>
+				{/* <img src="../static/image/logo.png" alt="my image" /> */}
+				<Header />
+			</Col>
+		</Row>
+		<Row>
+			<Col />
+		</Row>
+		<Row>
+			<Col md={3}>
+				<PopularRecipeList />
+			</Col>
+			<Col md={9}>
+				<MainItemsBlock />
+				<VideoList />
+			</Col>
+		</Row>
+	</Container>
 )
 
 export default Layout
