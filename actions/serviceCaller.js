477099c86d0efb3b2983b7422bb66e0076bfb1fc
Meghdad Hadidi <meghdad.1987@gmail.com>
2019-02-06 17:16:32 +0330

    changed menu item service

--- actions/serviceCaller.js
+++ actions/serviceCaller.js
@@ -1,17 +1,17 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: "http://bejooi.ir/api/v1",
-    headers: {
-        "Content-Type": "application/json;charset=UTF-8"
-    }
+	baseURL: "bejooi.ir/api/v1",
+	headers: {
+		"Content-Type": "application/json;charset=UTF-8"
+	}
 })
 
 export const getMenuItemsService = params =>
-    instance.get("/menus?id=1", { params })
+	instance.get("/menus?id=1", { params })
 
 export const getRecipeItemsService = params =>
-    instance.get("/5be417132f00005c00d9f319", { params })
+	instance.get("/5be417132f00005c00d9f319", { params })
 
 export const getVideosService = params =>
-    instance.get("/5be442002f00008f00d9f442", { params })
+	instance.get("/5be442002f00008f00d9f442", { params })
