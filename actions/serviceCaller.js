f49bd3368fc6a9e3c136507de0a23ea88aad4425
Meghdad Hadidi <meghdad.1987@gmail.com>
2019-02-16 22:50:21 +0330

    changed the mocky base URL to https protocol

--- actions/serviceCaller.js
+++ actions/serviceCaller.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-	baseURL: "http://www.mocky.io/v2",
+	baseURL: "https://www.mocky.io/v2",
 	headers: {
 		"Content-Type": "application/json;charset=UTF-8"
 	}
